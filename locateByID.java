fab66a9ceb16413912ec872431c491b0813abb52
mehamahajan <84273800+mehamahajan@users.noreply.github.com>
2022-06-25 22:14:23 +0530

    uploaded sample .java file

---
+++ locateByID.java
@@ -0,0 +1,21 @@
+package locateByID;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class locateByID {
+	 public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","D:\\Meha\\Selenium\\Drivers\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();
+	
+    String baseUrl = "https://www.facebook.com/";
+    driver.get(baseUrl);
+    String tagName = "";
+    tagName = driver.findElement(By.id("email")).getTagName();
+    System.out.println(tagName);
+    driver.close();
+    System.exit(0);
+    
+}
+	 
+}
